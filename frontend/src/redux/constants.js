1e982d66ba0b9d2f3e926f8cf4f3273a4e64b602
karthickrajaofficial <karthickrajaofficial1@gmail.com>
2024-06-21 13:40:17 +0530

    Initial commit

---
+++ frontend/src/redux/constants.js
@@ -0,0 +1,9 @@
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+export const BASE_URL = ""
+export const USERS_URL = "/api/users"
+export const CATEGORY_URL = "/api/category";
+export const PRODUCT_URL = "/api/products";
+export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
+// export const PAYPAL_URL = "/api/config/paypal";
+export const RAZORPAY_URL = "/api/config/payment";
